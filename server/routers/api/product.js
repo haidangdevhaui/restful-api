01cd1ddd4bdf789ca0b9692c531157a03d6c1eb6
haidangdevhaui <haidangdevhaui@gmail.com>
2017-03-03 14:36:28 +0700

    u

--- server/routers/api/product.js
+++ server/routers/api/product.js
@@ -14,6 +14,7 @@ router.get('/', (req, res) => {
 
 router.post('/create', (req, res) => {
     let product = new Product(req.body);
+    console.log(product);
     product.save((err, _product) => {
         return res.json({
             error: err ? false : err,
