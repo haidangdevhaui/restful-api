056634bf92851cdb5c59cf7ad1523c3fed12503f
haidangdevhaui <haidangdevhaui@gmail.com>
2017-03-13 15:57:00 +0700

    u

--- server/index.js
+++ server/index.js
@@ -6,8 +6,9 @@ import methodOverride from 'method-override';
 import session from 'express-session';
 import passport from 'passport';
 import cors from 'cors';
+/*
 import herokuProxy from 'heroku-proxy';
-
+*/
 import configServer from './config/server';
 import configDB from './config/database';
 
@@ -53,12 +54,14 @@ app.use(webpackMiddleware(compiler, {
 }));
 app.use(webpackHotMiddleware(compiler));
 
+/*
 app.use(herokuProxy({
   hostname: 'api-haidangdev.herokuapp.com',
   port    : configServer.port,
   prefix  : '/',
   protocol: 'http'
 }));
+*/
 
 app.use('/static', express.static('public'))
 app.use(cors());
